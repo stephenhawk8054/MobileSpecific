b99f6c11a63b7acb00e23c6485f176d5ee4ecf39
stephenhawk8054 <nguyen_ftorres@yahoo.com>
2023-04-30 11:20:16 +0700

    optimize code

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-from collections import Counter
-
 from utils import load_text, write_json
 
 
 def main():
-    text_file = 'mobile_specific.txt'
-    ubo_stats = dict()
-    for line in load_text(text_file, is_list=True):
+    stats = dict()
+    for line in load_text('mobile_specific.txt', is_list=True):
         if line.startswith('!'):
             continue
 
-        ubo_filters = (line_split := line.split('##'))[1].split(',')
-        domains: list[str] = line_split[0].split(',')
-        
-        for ubo_filter in ubo_filters:
-            if ubo_filter not in ubo_stats:
-                ubo_stats[ubo_filter] = {
-                    'number': len(domains),
-                    'domain': ','.join(sorted(domains))
-                }
-            else:
-                current_domain = ubo_stats[ubo_filter]['domain'].split(',')
-                domains.extend(current_domain)
+        domains = (line_split := line.split('##'))[0].split(',')
+        for filter in line_split[1].split(','):
+            if filter in stats:
+                domains.extend(stats[filter]['domain'].split(','))
 
-                ubo_stats[ubo_filter]['domain'] = ','.join(sorted(domains))
-                ubo_stats[ubo_filter]['number'] = len(domains)
+            stats[filter] = {
+                'number': len(domains),
+                'domain': ','.join(sorted(domains))
+            }
 
-    ubo_stats = dict(sorted(ubo_stats.items(), key=lambda item: item[1]['number'], reverse=True))
-    write_json(ubo_stats, 'stats.json')
+    stats = dict(sorted(stats.items(), key=lambda item: item[1]['number'], reverse=True))
+    write_json(stats, 'stats.json')
 
 
 if __name__ == "__main__":
